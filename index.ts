dacfbaa73a241a36693a6359ac577166a16e5c89
Akshay Parmar <akshay.parmar@softwebsolutions.com>
2025-07-21 16:20:35 +0530

    feat: integrate socket.io with Hono server and remove Bun server implementation

--- index.ts
+++ index.ts
@@ -2,27 +2,30 @@ import { Hono } from "hono";
 import routes from "./src/routers/router";
 import APIEndpoints from "./src/constants/apiEndpoints";
 import { Server } from "socket.io";
+import { createServer } from "http2";
+import { serve, createAdaptorServer } from "@hono/node-server";
 import Constants from "./src/constants/constants";
 
 const app = new Hono();
 app.route(APIEndpoints.API!, routes);
 
-const bunServer = Bun.serve({
-  fetch: app.fetch,
-  port: 4000,
-  websocket: {
-    open(ws) {
-      console.log("Client connected");
-      ws.send("Hello from server");
-    },
+const httpServer = createAdaptorServer(app);
 
-    message(ws, message) {
-      console.log("Received from client:", message);
-      ws.send(`Echo: ${message}`);
-    },
+// const bunServer = Bun.serve({
+//   fetch: app.fetch,
+//   port: 4000,
+// });
 
-    close(ws) {
-      console.log("Client disconnected");
-    },
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
   },
+});
+
+io.on(Constants.CONNECTION, () => {
+  console.log("socket.io");
+});
+
+httpServer.listen(4000, () => {
+  console.log("Server Started");
 });
