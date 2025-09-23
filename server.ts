c04708541e45915845abd702688c0ed40c23c2be
Akshay Parmar <akshay.parmar@softwebsolutions.com>
2025-09-23 16:37:32 +0530

    Working on new connection protocol

--- server.ts
+++ server.ts
@@ -1,11 +1,22 @@
-import { createAdaptorServer } from "@hono/node-server";
+import { serve } from '@hono/node-server'
+import { createServer } from 'node:https'
+import fs from 'node:fs'
+
 import io from "./src/sockets";
 import app from "./src/app";
+
 import { getServerPort } from "./src/services/env.service";
 
 const port = getServerPort();
 
-const server = createAdaptorServer(app);
+const server = serve({
+  fetch: app.fetch,
+  createServer: createServer,
+  serverOptions: {
+    key: fs.readFileSync('D:/ssl certificates/server-ec.key'),
+    cert: fs.readFileSync('D:/ssl certificates/server-ec.crt'),
+  },
+})
 
 io.attach(server);
 
